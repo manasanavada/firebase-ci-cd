749b82d4c0e0d0382eec3a94ac923dcfb4ad7e29
Manasa <manasa@Manasas-MBP-2.fios-router.home>
2021-03-20 21:40:50 -0400

    update function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,5 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
   // eslint-disable-next-line
   functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+  response.send('Hello from Manasa!');
 });
